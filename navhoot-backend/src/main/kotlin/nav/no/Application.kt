c809094d8860d834100c4fa5747bae0133d4acbd
Henrik Horten Hegli <henrik.horten.hegli@nav.no>
2022-07-07 13:16:35 +0200

    initiated repo and added readme and backend

---
+++ navhoot-backend/src/main/kotlin/nav/no/Application.kt
@@ -0,0 +1,12 @@
+package nav.no
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import nav.no.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
