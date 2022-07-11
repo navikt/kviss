3051609d978aab92fb4a759bf3c6b6ef69c9d812
EmelijonJ <53192344+EmelijonJ@users.noreply.github.com>
2022-07-11 17:48:09 +0200

    Url name changed

--- backend/src/main/kotlin/nav/no/database/connector.kt
+++ backend/src/main/kotlin/nav/no/database/connector.kt
@@ -4,16 +4,15 @@ import io.ktor.network.sockets.*
 import java.sql.DriverManager
 
 
-val dbUrl = System.getenv("NAIS_DATABASE_NAVHOOT_DB_URL")
-
-public fun dbConnect(): String {
+val dbUrl = System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_URL")
+fun dbConnect(): String {
     val jdbcUrl = dbUrl
 
     val connection = DriverManager.getConnection(jdbcUrl)
 
-    if (connection.isValid(5000)) {
-        return "connection is good"
+    return if (connection.isValid(5000)) {
+        "connection is good"
     } else {
-        return "connection is bad"
+        "connection is bad"
     }
 }
