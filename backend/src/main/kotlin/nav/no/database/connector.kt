06b15da5b673ed1b84b9902c8818f464ff0a35bf
Henrik Horten Hegli <henrik.horten.hegli@nav.no>
2022-07-12 11:37:57 +0200

    updated env fields and added flyway

--- backend/src/main/kotlin/nav/no/database/connector.kt
+++ backend/src/main/kotlin/nav/no/database/connector.kt
@@ -3,6 +3,7 @@ package nav.no.database
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.network.sockets.*
+import org.flywaydb.core.Flyway
 import java.sql.DriverManager
 import javax.sql.DataSource
 
@@ -12,10 +13,10 @@ class DataSourceBuilder(private val env: Map<String, String>) {
 private val hikariConfig = HikariConfig().apply {
     jdbcUrl = String.format(
         "jdbc:postgresql://%s:%s/%s%s",
-        requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_HOST")) { "database host must be set if jdbc url is not provided" },
-        requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_PORT")) { "database port must be set if jdbc url is not provided" },
-        requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_DATABASE")) { "database name must be set if jdbc url is not provided" },
-        System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_USERNAME")?.let { "?user=$it" } ?: "")
+        requireNotNull(env["NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_HOST"]) { "database host must be set if jdbc url is not provided" },
+        requireNotNull(env["NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_PORT"]) { "database port must be set if jdbc url is not provided" },
+        requireNotNull(env["NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_DATABASE"]) { "database name must be set if jdbc url is not provided" },
+        env["NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_USERNAME"]?.let { "?user=$it" } ?: "")
 
     env["NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_USERNAME"]?.let { this.username = it }
     env["NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_PASSWORD"]?.let { this.password = it }
@@ -24,4 +25,15 @@ private val hikariConfig = HikariConfig().apply {
     init {
         dataSource = HikariDataSource(hikariConfig)
     }
+    fun migrate() =  runMigration(dataSource)
+
+
+    private fun runMigration(dataSource: DataSource) =
+        Flyway.configure()
+            .dataSource(dataSource)
+            .apply {
+                if (env.containsKey("NAIS_CLUSTER_NAME")) locations("db/migration", "db/gcp")
+            }
+            .load()
+            .migrate()
 }
