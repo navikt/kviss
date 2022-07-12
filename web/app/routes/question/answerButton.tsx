5d3b9d7a0164a0fd6948da8ee03f09cd5c3bb430
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-07-12 12:48:35 +0200

    Add rendering of button array

--- web/app/routes/question/answerButton.tsx
+++ web/app/routes/question/answerButton.tsx
@@ -1,7 +1,16 @@
-export default function AnswerButton({question}: {question: string}) {
+import type { ChangeEvent } from "react"
+
+export default function AnswerButton(
+    {
+        answerText, 
+        onAnswerClicked
+    }: {
+        answerText: string
+        onAnswerClicked: () => void
+    }) {
     return (
-        <button className="border mx-1">
-            {question}
+        <button className="border mx-1" onClick={onAnswerClicked} >
+            {answerText}
         </button>
     )
 }
--- web/app/routes/question/index.tsx
+++ web/app/routes/question/index.tsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import AnswerButton from "./answerButton";
+
+interface IAnswer {
+    answerText: string
+    isCorrectAnswer: boolean
+}
 
 export default function QuestionView() {
 
-    
+    const onQuestionAnswered = () => {
+        console.log("Hei")
+    }
+
+    const [answers, setAnswers] = useState<IAnswer[]>([
+        {
+            answerText: "Question A",
+            isCorrectAnswer: false
+        },
+        {
+            answerText: "Question B",
+            isCorrectAnswer: false
+        },
+        {
+            answerText: "Question C",
+            isCorrectAnswer: true
+        },
+        {
+            answerText: "Question D",
+            isCorrectAnswer: false
+        }
+    ])
 
     return (
         <div>
-            <AnswerButton question="Question A" />
-            <AnswerButton question="Question B" />
-            <AnswerButton question="Question C" />
-            <AnswerButton question="Question D" />
+            {answers.map((answer, i) => {
+                return <AnswerButton 
+                    key={i} 
+                    answerText={answer.answerText} 
+                    onAnswerClicked={onQuestionAnswered}
+                />
+            })}
         </div>
     )
 }
