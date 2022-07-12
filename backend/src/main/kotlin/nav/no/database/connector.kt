6c2bfc9f6f67ff6a3c25aba2cb2ba57801a7e70c
Henrik Horten Hegli <henrik.horten.hegli@nav.no>
2022-07-12 09:53:10 +0200

    changed db url again

--- backend/src/main/kotlin/nav/no/database/connector.kt
+++ backend/src/main/kotlin/nav/no/database/connector.kt
@@ -4,13 +4,22 @@ import io.ktor.network.sockets.*
 import java.sql.DriverManager
 
 
-val dbUrl = System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_URL")
+class Connector()
+
+val dbUrl = System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_URL")?: String.format(
+    "jdbc:postgresql://%s:%s/%s%s",
+    requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_HOST")) { "database host must be set if jdbc url is not provided" },
+    requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_PORT")) { "database port must be set if jdbc url is not provided" },
+    requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_URL")) { "database name must be set if jdbc url is not provided" },
+    System.getenv("DB_USERNAME")?.let { "?user=$it" } ?: "")
+
 fun dbConnect(): String {
-    val connection = DriverManager.getConnection(dbUrl)?: String.format(
-        "jdbc:postgresql://%s:%s/%s%s",
-        requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_HOST")) { "database host must be set if jdbc url is not provided" },
-        requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_PORT")) { "database port must be set if jdbc url is not provided" },
-        requireNotNull(System.getenv("NAIS_DATABASE_NAVHOOT_BACKEND_NAVHOOT_DB_URL")) { "database name must be set if jdbc url is not provided" },
-        System.getenv("DB_USERNAME")?.let { "?user=$it" } ?: "")
-    return connection.toString()
+    val connection = DriverManager.getConnection(dbUrl)
+
+    if (connection.isValid(20)){
+        return "hello"
+    } else {
+        return "not valid"
+    }
 }
+
