b407d2734255f86c13edb6ee667f50d5a548680a
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-07-13 14:37:20 +0200

    Add dynamic routing on quiz questions

--- web/app/routes/question/$slug.tsx
+++ web/app/routes/question/$slug.tsx
@@ -3,11 +3,18 @@ import { useLoaderData } from "@remix-run/react";
 import QuestionView from ".";
 
 export const loader = async ({ params }: {params: any}) => {
-    return json({ slug: params.slug })
+    const res = await fetch("http://navhoot-backend.dev.nav.no/quiz")
+    
+    const resAsJSON = await res.json()
+    
+    return json({
+        question: resAsJSON.questions[parseInt(params.slug) - 1],
+        slug: params.slug
+        })
 }
 
 export default function QuestionSlug() {
-    const { slug } = useLoaderData()
+    const {question, slug} = useLoaderData()
 
-    return <>{slug}</>
+    return <QuestionView description={question.description} alternative={question.alternative} questionIndex={slug}/>
 }
--- web/app/routes/question/index.tsx
+++ web/app/routes/question/index.tsx
@@ -1,36 +1,43 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useNavigate } from "@remix-run/react";
 import AnswerButton from "./answerButton";
 
-export function CatchBoundary() {}
-
-export const loader: LoaderFunction = async () => {
-    const res = await fetch("http://navhoot-backend.dev.nav.no/quiz")
-    return json(await res.json())
+interface IQuestionViewProps {
+    description: string
+    alternative: IAlternative[]
+    questionIndex: number
 }
 
-export default function QuestionView() {
-    const quiz = useLoaderData();
-    
+interface IAlternative {
+    text: string
+    isCorrect: boolean
+}
+
+export default function QuestionView({
+    description,
+    alternative,
+    questionIndex
+}: IQuestionViewProps) {   
+    const navigate = useNavigate()
+
     const onQuestionAnswered = (answerIndex: number) => {
-        if (quiz.questions[0].alternative[answerIndex].isCorrect) {
+        if (alternative[answerIndex].isCorrect) {
             // TODO: Route to result screen with displaying that answer is correct
-            //navigate('./result', { replace: true })
+            //navigate(`./result`, { replace: true })
         }
         // TODO: Route to to result screen with displaying that answer is incorrect
     }
-    if (!quiz?.questions) return <></>
     return (
         <div className="flex flex-col h-screen justify-center items-center">
-            <h1 className="text-2xl mb-4">{quiz.questions[0].description}</h1>
-            {quiz ? quiz.questions[0].alternative.map((answer: any, i: number) => {
+            <h1 className="text-2xl mb-4">{description}</h1>
+            {alternative.map((answer: any, i: number) => {
                 return <AnswerButton 
                     key={i}
                     answerText={answer.text} 
                     onClick={() => onQuestionAnswered(i)}
                 />
-            }): <div>Hello</div> }
+            })}
         </div>
     )
 }
