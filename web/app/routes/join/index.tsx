732950ca7cf2655d3bc2bfa308ab929e05783d97
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-07-14 10:04:47 +0200

    Fix indentation

--- web/app/routes/join/index.tsx
+++ web/app/routes/join/index.tsx
@@ -1,56 +1,56 @@
-import { json, LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { useState } from "react";
-import PinCode from "~/components/PinCode";
-import Username from "~/components/Username";
-import QuizProvider, { useQuiz } from "~/context/QuizContext";
+import { json, LoaderFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { useState } from 'react'
+import PinCode from '~/components/PinCode'
+import Username from '~/components/Username'
+import QuizProvider, { useQuiz } from '~/context/QuizContext'
 
 
 export type ButtonProps = {
-  handleClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, pinCode: String) => void;
+  handleClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, pinCode: string) => void;
 };
 
 export const loader: LoaderFunction = async () => {
-  const res = await fetch("https://navhoot-backend.dev.nav.no/quiz")
-  return json(await res.json())
-};
+	const res = await fetch('https://navhoot-backend.dev.nav.no/quiz')
+	return json(await res.json())
+}
 
 
 export default function QuizIndexRoute() {
 
 
-    const [pinCode, setPinCode] = useState<String>("")
-    const [nickname, setNickName] = useState<String>("")
+	const [pinCode, setPinCode] = useState<string>('')
+	const [nickname, setNickName] = useState<string>('')
 
-    const {quiz, setQuiz } = useQuiz()
-    const data = useLoaderData()
+	const {quiz, setQuiz } = useQuiz()
+	const data = useLoaderData()
 
-    const handleClickPin = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, pc: String) => {
-      event.preventDefault()
-      setPinCode(pc)
-    }
+	const handleClickPin = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, pc: string) => {
+		event.preventDefault()
+		setPinCode(pc)
+	}
 
-    const handleClickNick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, pc: String) => {
-      event.preventDefault()
-      setNickName(pc)
-      setQuiz(data)
+	const handleClickNick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, pc: string) => {
+		event.preventDefault()
+		setNickName(pc)
+		setQuiz(data)
 
-  }
+	}
 
-    return (
-        <div className="flex flex-col h-screen justify-center items-center">
+	return (
+		<div className="flex flex-col h-screen justify-center items-center">
         
-          {pinCode === ""? 
-            <PinCode handleClick={handleClickPin}/>:
-            (nickname ===""?
-              <Username handleClick={handleClickNick}/>
-              : 
-              <div className="text-center ">
+			{pinCode === ''? 
+				<PinCode handleClick={handleClickPin}/>:
+				(nickname ===''?
+					<Username handleClick={handleClickNick}/>
+					: 
+					<div className="text-center ">
                 See your name on the screen?
-                <br/>
-                {nickname}
-              </div>
-            )}
-        </div>
-    );
-  }
+						<br/>
+						{nickname}
+					</div>
+				)}
+		</div>
+	)
+}
