55120dd4e8795dcfe1f45d747efec69014fdc246
ivarhe <70262276+ivarhe@users.noreply.github.com>
2022-07-14 14:45:34 +0200

    fixed linting

--- web/app/routes/question/$slug.tsx
+++ web/app/routes/question/$slug.tsx
@@ -5,17 +5,17 @@ import { useQuiz } from '~/context/QuizContext'
 
 
 export const loader = async ({ params }: { params: any }) => {
-	return json({
-		slug: params.slug
-	})
+    return json({
+        slug: params.slug
+    })
 }
 
 export default function QuestionSlug() {
-	const { quiz } = useQuiz()
-	const { slug } = useLoaderData()
+    const { quiz } = useQuiz()
+    const { slug } = useLoaderData()
 
-	const question = quiz.questions[parseInt(slug) - 1]
+    const question = quiz.questions[parseInt(slug) - 1]
 
 
-	return <QuestionView description={question.description} alternative={question.alternative} questionIndex={slug} />
+    return <QuestionView description={question.description} alternative={question.alternative} questionIndex={slug} />
 }
