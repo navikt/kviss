9679060973b1e1aa3266d7a2e3b01a26b033baf9
NN <nicolas.nordhagen@nav.no>
2022-07-14 15:58:08 +0200

    update postgres role cloudsqliamuser

--- backend/src/main/resources/db/migration/V1__init_db.sql
+++ backend/src/main/resources/db/migration/V1__init_db.sql
@@ -1,3 +1,14 @@
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (select 1 from pg_roles where rolname = 'cloudsqliamuser')
+        THEN
+            grant all on all tables in schema public to cloudsqliamuser;
+        END IF;
+    END
+$$;
+
 create table quiz
 (
     id          bigserial
