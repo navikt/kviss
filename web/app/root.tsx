b2a289aaa90d0872d5116ea893282f21d6da618d
ivarhe <70262276+ivarhe@users.noreply.github.com>
2022-07-14 09:35:20 +0200

    updated title and favicon

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -17,7 +17,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "NavHoot",
   viewport: "width=device-width,initial-scale=1",
 });
 
