16c0b3bbbcbeac317354ab2fb3603933ac77b683
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-07-15 15:04:23 +0200

    Add data collection on quiz

--- web/app/routes/quiz/createQuiz.tsx
+++ web/app/routes/quiz/createQuiz.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from 'react'
-import { IAlternative, IQuestion } from '~/context/QuizContext'
+import { IAlternative, IQuestion, IQuiz } from '~/context/QuizContext'
 
 interface IQuizInfo {
     name: string
@@ -20,7 +20,11 @@ export default function CreateQuiz() {
         description: ''
     })
 
+    const [questionDescription, setQuestionDescription] = useState<string>('')
+
     const [questions, setQuestions] = useState<IQuestion[]>([])
+
+    const [quiz, setQuiz] = useState<IQuiz>()
 
     const handleQuizInfoChange = (event: ChangeEvent<HTMLInputElement>) => {
         setQuizInfo({
@@ -29,10 +33,37 @@ export default function CreateQuiz() {
         })
     }
 
-    const replaceAlternativeAtIndex = (index: number, newAlternativeText: string) => {
+    const handleQuestionDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setQuestionDescription(event.target.value)
+    }
+
+    const replaceAlternativeTextAtIndex = (index: number, newAlternativeText: string) => {
         const alternatives = [...tempAlternativesArray]
         alternatives[index] = {id: alternatives[index].id, text: newAlternativeText, isCorrect: alternatives[index].isCorrect}
         setTempAlternativesArray(alternatives)
+    }
+
+    const replaceAlternativeIsCorrectAtIndex = (index: number, newAlternativeIsCorrect: boolean) => {
+        const alternatives = [...tempAlternativesArray]
+        alternatives[index] = {id: alternatives[index].id, text: alternatives[index].text, isCorrect: newAlternativeIsCorrect}
+        setTempAlternativesArray(alternatives)
+    }
+
+    const onAddQuestion = () => {
+        setQuestions(questions.concat({
+            id: questions.length,
+            description: questionDescription,
+            alternative: tempAlternativesArray
+        }))
+    }
+    
+    const onCreateQuiz = () => {
+        setQuiz({
+            id: 1,
+            name: quizInfo.name,
+            description: quizInfo.description,
+            questions
+        })
     }
 
     return (
@@ -50,23 +81,41 @@ export default function CreateQuiz() {
             </form>
             <h2 className='text-2xl my-2'>Questions</h2>
             <div>
-                {/** PUT ALL FILLED OUT QUESTIONS HERE */}
+                {questions.map((item, i) => {
+                    return (
+                        <div key={i}className='flex flex-col my-2'>
+                            <p>Question: {item.description}</p>
+                            {item.alternative.map((alt, j) => {
+                                return <p key={j}>{`Alternative ${j + 1}: ${alt.text}. Correct?: ${alt.isCorrect}`}</p>
+                            })}
+                        </div>
+                    )
+                })}
             </div>
             <form className='flex flex-col'>
                 <label className='mb-1'>
                     Description:
-                    <input type="text" name="description"/>
+                    <input type="text" onChange={handleQuestionDescriptionChange}/>
                 </label>
                 {tempAlternativesArray.map((item, i) => {
                     return (
-                        <label key={i} className="my-1">
-                            {`Alternative ${i + 1}:`}
-                            <input type="text" onChange={e => replaceAlternativeAtIndex(i, e.target.value)} />
-                        </label>
+                        <div key={i} className="my-1">
+                            <label>
+                                {`Alternative ${i + 1}:`}
+                                <input type="text" onChange={e => replaceAlternativeTextAtIndex(i, e.target.value)} />
+                            </label>
+                            <label className='ml-2'>
+                                Correct?:
+                                <input type="checkbox" onChange={e => replaceAlternativeIsCorrectAtIndex(i, e.target.checked)} />
+                            </label>
+                        </div>
                     )
                 })}
             </form>
-            <button className='border mt-2'>Add question</button>
+            <button className='border mt-2' onClick={onAddQuestion}>Add question</button>
+            <button className='flex flex-col' onClick={onCreateQuiz}>
+                <h1 className='text-2xl my-2'>CREATE QUIZ</h1>
+            </button>
         </div>
     )
 }
