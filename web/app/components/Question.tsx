a1626f7d4db76fcec3936ce06d8d948901e816e3
ivarhe <70262276+ivarhe@users.noreply.github.com>
2022-07-15 12:57:24 +0200

    Added toggleScoreboard

--- web/app/components/Question.tsx
+++ web/app/components/Question.tsx
@@ -1,16 +1,22 @@
-import { IQuestion, useQuiz } from "~/context/QuizContext"
-import AnswerButton from "./answerButton"
+import { IQuestion, ScoreboardProps, useQuiz } from "~/context/QuizContext"
+import AnswerButton from "./AnswerButton"
 
 
-export function Question({ description, alternative, id }: IQuestion) {
-
+export function Question(tb: ScoreboardProps): JSX.Element {
 
     const getIndex = (i: number | undefined): number => {
+
+        if (i ? i > quiz.questions.length : true) {
+            return -1
+        }
         return quiz.questions.findIndex((obj: IQuestion) => obj.id === i)
     }
 
     const { setQuestion, question, quiz } = useQuiz()
 
+    const alternative = question.alternative
+    const id = question.id
+    const description = question.description
 
     const onQuestionAnswered = (answerIndex: number) => {
         if (alternative?.[answerIndex].isCorrect) {
@@ -18,12 +24,16 @@ export function Question({ description, alternative, id }: IQuestion) {
             //navigate(`./result`, { replace: true })
         }
         // TODO: Route to to result screen with displaying that answer is incorrect
-        if (getIndex(id ? id + 1 : undefined)) {
-            setQuestion(id ? quiz.questions[id + 1] : 1)
+        if (getIndex(id ? id + 1 : -1)) {
+            if (getIndex(id ? id + 1 : -1) === -1) {
+                tb.toggleScoreboard(true)
+                return
+            }
+            setQuestion(id ? quiz.questions[id] : 1)
+            tb.toggleScoreboard(true)
         }
 
     }
-
 
     return (
         <div className="flex flex-col h-screen justify-center items-center">
