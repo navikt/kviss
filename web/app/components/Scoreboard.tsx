49eb68b52fa7d6398ef74192ab93fdbbb981d281
ivarhe <70262276+ivarhe@users.noreply.github.com>
2022-07-15 12:57:47 +0200

    Updated scoreboard

--- web/app/components/Scoreboard.tsx
+++ web/app/components/Scoreboard.tsx
@@ -1,23 +1,23 @@
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
-import { useQuiz } from '~/context/QuizContext'
+import { ScoreboardProps, useQuiz } from '~/context/QuizContext'
 
 
-export const loader = async ({ params }: { params: any }) => {
-    return json({
-        quizId: params.quizId
-    })
-}
+export default function Scoreboard(tb: ScoreboardProps): JSX.Element {
 
-export default function scoreBoardSlug() {
     const { quiz } = useQuiz()
-    const { quizId } = useLoaderData()
 
-    console.log(quiz)
-
+    const onNameClick = () => {
+        tb.toggleScoreboard(false)
+    };
 
     return <>
-        scoreBoard
-
+        <div className="flex flex-col h-screen justify-center items-center">
+            <button
+                className="border-1 rounded-3xl my-2 p-1.5 w-40 h-20 bg-nord-grey drop-shadow-lg"
+                onClick={() => onNameClick()}>
+                Neste spørsmål
+            </button>
+        </div>
     </>
 }
