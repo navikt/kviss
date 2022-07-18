eb90fd25e0f34683e331d87dd3d79dbe56ab113d
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-07-18 10:37:28 +0200

    Add buttons for quiz options

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -2,6 +2,16 @@ import { Outlet } from '@remix-run/react'
 
 export default function Index() {
     return (
-        <Outlet />
+        <div className='flex items-center justify-center h-screen flex-col'>
+            <button className='border-2 rounded-lg bg-white my-4 py-4 w-64'>
+                <p className='text-2xl'>Join quiz</p>
+            </button>
+            <button className='border-2 rounded-lg bg-white my-4 py-4 w-64'>
+                <p className='text-2xl'>Start quiz</p>
+            </button>
+            <button className='border-2 rounded-lg bg-white my-4 py-4 w-64'>
+                <p className='text-2xl'>Create quiz</p>
+            </button>
+        </div>
     )
 }
