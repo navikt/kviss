a0661c827523bc6ce86e777d47a51be6d74146de
Henrik Horten Hegli <henrik.horten.hegli@nav.no>
2022-07-19 07:57:55 +0200

    Now sends questions based on quiz id using ws, missing serializer for quiz so can't send that yet

--- backend/src/main/kotlin/nav/no/Application.kt
+++ backend/src/main/kotlin/nav/no/Application.kt
@@ -5,15 +5,20 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.websocket.*
 import nav.no.database.DataSourceBuilder
+import nav.no.database.navhootDao.AlternativesDao
+import nav.no.database.navhootDao.QuestionDao
+import nav.no.database.navhootDao.QuizDao
 import nav.no.plugins.*
+import nav.no.services.QuizService
 import java.time.Duration
 
 fun main() {
     val datasourceBuilder = DataSourceBuilder(System.getenv())
 //    datasourceBuilder.migrate()
+    val quizService = QuizService(QuestionDao(datasourceBuilder.dataSource), QuizDao(datasourceBuilder.dataSource), AlternativesDao(datasourceBuilder.dataSource))
 
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting(datasourceBuilder.dataSource)
-        configureSockets()
+        configureSockets(quizService)
     }.start(wait = true)
 }
--- backend/src/main/kotlin/nav/no/plugins/Sockets.kt
+++ backend/src/main/kotlin/nav/no/plugins/Sockets.kt
@@ -8,11 +8,12 @@ import io.ktor.websocket.*
 import kotlinx.serialization.json.Json
 import nav.no.models.CreateQuizRequest
 import nav.no.models.SocketConnection
+import nav.no.services.QuizService
 import java.time.Duration
 import java.util.*
 import kotlin.collections.LinkedHashSet
 
-fun Application.configureSockets() {
+fun Application.configureSockets(quizService: QuizService) {
     install(WebSockets) {
         pingPeriod = Duration.ofSeconds(20)
         timeout = Duration.ofSeconds(20)
@@ -27,15 +28,16 @@ fun Application.configureSockets() {
         webSocket("/chat/{id}") {
             println("Adding player!")
             val thisConnection = SocketConnection(this, call.parameters["id"]!!.toInt())
+            val param = call.parameters["id"]!!.toLong()
             connections += thisConnection
             try {
-                send("You are connected to WS ${call.parameters["id"]!!.toInt()}")
+                send("You are connected to WS ${call.parameters["id"]!!.toLong()}")
+                sendSerialized(quizService.getQuestions(param))
                 for (frame in incoming) {
                     frame as? Frame.Text ?: continue
                     val receivedText = frame.readText()
                     val textWithUsername = "[${thisConnection.name}]: $receivedText"
                     connections.filter { thisConnection.id == it.id }.forEach {
-                        println(frame)
                         it.session.send(textWithUsername)
                     }
                 }
