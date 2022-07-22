9f2bcd298409f56e40ee3178d8584a3bdea9772a
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-07-22 13:14:09 +0200

    Add poster function

--- web/app/api/hooks/useQuiz.ts
+++ web/app/api/hooks/useQuiz.ts
@@ -5,5 +5,9 @@ import { fetcher } from '../operations'
 export const useQuiz = (id: number) => {
     const { data, error } = useSWR<IQuiz, Error>(`/quiz/${id}`, fetcher)
 
-    return { quiz: data, loading: !error && !data, error }
+    return {
+        quiz: data,
+        loading: !error && !data,
+        error,
+    }
 }
--- web/app/api/operations.ts
+++ web/app/api/operations.ts
@@ -9,3 +9,16 @@ export const fetcher = async (url: string) =>
         .catch((error: Error) => {
             throw error
         })
+
+export const poster = async <T>(url: string, data: T) => {
+    await fetch(url, {
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res: Response) => res.json())
+        .catch((error: Error) => {
+            throw error
+        })
+}
