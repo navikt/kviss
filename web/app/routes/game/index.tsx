42fe07d367c78a025a4cb057dcde25e753ac1fde
ivarhe <70262276+ivarhe@users.noreply.github.com>
2022-07-22 16:34:52 +0200

    Henrik ville hjem :(

--- web/app/routes/game/index.tsx
+++ web/app/routes/game/index.tsx
@@ -1,19 +1,15 @@
-import { useEffect } from "react"
-import { useWebSocket } from "~/context/SocketContext"
+import { useGameContext } from "~/context/game/GameContext"
 
 
 export default function QuizView() {
 
-    const ws = useWebSocket()
+    const { state, dispatch } = useGameContext()
 
-    const send = () => {
-        ws?.send("Hello")
-    }
 
     return (
         <div className="flex flex-col h-screen justify-center items-center">
-            <button onClick={send}>
-                send2
+            <button>
+                {state.username}
             </button>
         </div>
     )
