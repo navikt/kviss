cc6243b38042931b81bd0dc4f9721dfe633999ce
Henrik Hegli <54811257+Henrhh@users.noreply.github.com>
2022-07-25 11:45:03 +0200

    Socket event handlers (#101)
    * added event for sending question
    
    * added additional events
    
    * Most events should be handled now (dont @ me)
    
    * removed unused import
    
    * Cleaned up events so they are more sensibly partitioned

--- backend/src/main/kotlin/nav/no/sockets/Events.kt
+++ backend/src/main/kotlin/nav/no/sockets/Events.kt
@@ -2,10 +2,7 @@ package nav.no.sockets
 
 import kotlinx.serialization.Serializable
 import nav.no.database.domain.GamePin
-import nav.no.models.ConsumerQuestion
-import nav.no.models.Game
-import nav.no.models.Player
-import nav.no.models.Question
+import nav.no.models.*
 
 enum class EventType {
     JOIN_GAME_EVENT,
@@ -15,6 +12,7 @@ enum class EventType {
     NEXT_QUESTION_EVENT,
     SEND_QUESTION_EVENT,
     SHOW_ALTERNATIVES_EVENT,
+    SEND_ALTERNATIVES_EVENT,
     SELECT_ANSWER_EVENT,
     END_GAME_EVENT,
 }
@@ -64,6 +62,29 @@ data class SendQuestionEvent(
     val question: ConsumerQuestion
 ): Event {
     override val type = EventType.SEND_QUESTION_EVENT
+}
+
+@Serializable
+data class ShowAlternativesEvent(
+    val questionId: Long
+) : Event{
+    override val type = EventType.SHOW_ALTERNATIVES_EVENT
+}
+
+@Serializable
+data class SendAlternativesEvent(
+    val alternatives: List<ConsumerAlternative>
+) : Event {
+    override val type = EventType.SEND_ALTERNATIVES_EVENT
+}
+
+
+@Serializable
+data class SelectAnswerEvent(
+    val alternativeId: Long,
+    val playerId: Long
+) : Event{
+    override val type = EventType.SELECT_ANSWER_EVENT
 }
 
 @Serializable
