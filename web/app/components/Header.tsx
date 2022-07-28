6b331401cee8740d6c8c1457f9ae31d118f8b175
EmelijonJ <53192344+EmelijonJ@users.noreply.github.com>
2022-07-28 09:05:12 +0200

    color change on logo

--- web/app/components/Header.tsx
+++ web/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="w-full bg-slate-600 flex justify-center py-6">
             <div>
-                <img src={logo} alt="kviss™" className="object-scale-down h-10 invert" />
+                <img src={logo} alt="kviss™" className="object-scale-down h-10" />
             </div>
         </header>
     )
