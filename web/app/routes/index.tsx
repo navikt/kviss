f2b01469b4b4c043fc74402c19e096180529e95a
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-08-01 13:47:23 +0200

    Revert

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -1,23 +1,22 @@
 import { useNavigate } from '@remix-run/react'
-import Button from '~/components/common/Button'
 
 export default function Index() {
     const navigate = useNavigate()
 
     return (
-        <div className="flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center h-screen flex-col">
             <h1 className="text-3xl text-center mb-4 text-white">
                 Welcome to kviss! A application that solves all your quizzzing needs at NAV
             </h1>
-            <Button>
-                <p className="text-2xl w-36">Join quiz</p>
-            </Button>
-            <Button>
-                <p className="text-2xl w-36">Start quiz</p>
-            </Button>
-            <Button>
-                <p className="text-2xl w-36">Create quiz</p>
-            </Button>
+            <button className="border-2 rounded-lg bg-white my-4 py-4 w-64" onClick={() => navigate('./join')}>
+                <p className="text-2xl">Join quiz</p>
+            </button>
+            <button className="border-2 rounded-lg bg-white my-4 py-4 w-64" onClick={() => navigate('./start')}>
+                <p className="text-2xl">Start quiz</p>
+            </button>
+            <button className="border-2 rounded-lg bg-white my-4 py-4 w-64" onClick={() => navigate('./create')}>
+                <p className="text-2xl">Create quiz</p>
+            </button>
         </div>
     )
 }
