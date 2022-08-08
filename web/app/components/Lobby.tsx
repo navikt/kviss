cb7a5fc1ed9c9ae56ed0f06d1af4983e0dd0bba1
EmelijonJ <53192344+EmelijonJ@users.noreply.github.com>
2022-08-08 13:23:37 +0200

    changes to return

--- web/app/components/Lobby.tsx
+++ web/app/components/Lobby.tsx
@@ -7,9 +7,9 @@ export default function LobbyView() {
         <>
             <h1 className={'text-2xl text-white mb-20'}>Pin-code: {state.pin}</h1>
 
-            <div className="grid place-items-center sm:grid-cols-4 md:grid-cols-6 gap-4 mb-20">
-                {players.map((player, i) => {
-                    return <h2 key={i} className="col-auto text-white">{player.name}</h2>
+            <div className='grid place-items-center sm:grid-cols-4 md:grid-cols-6 gap-4 mb-20'>
+                {state.players?.map((player, i) => {
+                    return <h2 key={i} className='col-auto text-white'>{player.name}</h2>
                 })}
             </div>
         </>
