bd7fdff720f4598e22fa56ad8c7b1901af701a9e
MatiasGranHenriksen <Matias.Gran-Henriksen@nav.no>
2022-08-09 13:20:41 +0200

    Grid view change

--- web/app/components/Lobby.tsx
+++ web/app/components/Lobby.tsx
@@ -4,18 +4,19 @@ import { useGameContext } from '../context/game/GameContext'
 export default function LobbyView() {
     const { state } = useGameContext()
 
-    const nPlayers = state.players?.length || 0
-    let smCol: number
-    let mdCol: number
 
-    nPlayers > 4 ? smCol = 4 : smCol = nPlayers
-    nPlayers > 6 ? mdCol = 6 : mdCol = nPlayers
+    // const nPlayers = state.players?.length || 0
+    // let smCol: number
+    // let mdCol: number
+
+    // nPlayers > 4 ? smCol = 4 : smCol = nPlayers
+    // nPlayers > 6 ? mdCol = 6 : mdCol = nPlayers
     
 
     return (
         <>
             <h1 className={'text-2xl text-white mb-20'}>Pin-code: {state.pin}</h1>
-            <div className={`grid place-items-center sm:grid-cols-${smCol} md:grid-cols-${mdCol} gap-2 mb-20`}>
+            <div className='grid place-items-center sm:grid-cols-4 md:grid-cols-6 gap-2 mb-20'>
                 {state.players?.map((player, i) => {
                     return <h2 key={i} className='col-auto text-white'>{player.name}</h2>
                 })}
