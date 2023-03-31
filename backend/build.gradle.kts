9ef9c8c105c097def9e14b256cb3a84a899b59a4
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-03-31 08:15:16 +0200

    Updated several libs

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,13 +1,18 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val kafkaVersion = "3.1.0"
+val kafka_version = "3.4.0"
+val postgresql_version = "42.5.4"
+val hikariCP_version = "3.4.5"
+val flyway_core_version = "6.5.0"
+val mockk_version = "1.13.4"
+val junit_jupiter_version = "5.9.2"
 
 
 plugins {
     application
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+    kotlin("jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
 group = "nav.no"
@@ -51,25 +56,23 @@ tasks {
 }
 repositories {
     mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
 dependencies {
     implementation("io.ktor:ktor-server-websockets:$ktor_version")
-
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.postgresql:postgresql:42.4.0")
-    implementation("com.zaxxer:HikariCP:3.4.5")
-    implementation("org.flywaydb:flyway-core:6.5.0")
+    implementation("org.postgresql:postgresql:$postgresql_version")
+    implementation("com.zaxxer:HikariCP:$hikariCP_version")
+    implementation("org.flywaydb:flyway-core:$flyway_core_version")
     implementation("io.ktor:ktor-server-cors:$ktor_version")
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("io.mockk:mockk:$mockk_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit_jupiter_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junit_jupiter_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junit_jupiter_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
