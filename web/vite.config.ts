354135f7201f2dac350e09883fd4d77fa6da7240
albrektsson <emil.albrektsson@nav.no>
2024-04-11 12:34:14 +0200

    Suffix to docker images, disabled process.env in vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    "process.env": process.env,
+    //"process.env": process.env,
   },
   plugins: [react()],
 });
