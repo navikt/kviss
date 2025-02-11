db87c663bdb5655afa23d8646400acb82e390a86
albrektsson <emil.albrektsson@nav.no>
2025-02-11 11:35:29 +0100

    Add jsonwebtoken to the correct project

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    //"process.env": process.env,
-  },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['jsonwebtoken'],
+    },
+  },
 });
